3d7cb438ca116041ab4cd7bb0094649559810ceb
Steve Howell <steve@strings.com>
2011-08-26 14:23:29 -0700

    starting on toggle

--- json_wiki.coffee
+++ json_wiki.coffee
@@ -6,10 +6,12 @@ Atom = (s) ->
     value: -> elem.val()
     
 ListRawView = (array) ->
+  div = $("<div>")
   textarea = $("<textarea>").attr("rows", 10)
   textarea.html JSON.stringify(array, null, " ")
+  div.append textarea
   self =
-    element: -> textarea
+    element: -> div
     value: -> JSON.parse textarea.html()
     
 ListEditView = (subwidgets) ->
@@ -21,30 +23,53 @@ ListEditView = (subwidgets) ->
     element: -> ul
     value: -> _.map subwidgets, (w) -> w.value()
 
-MultiView = (widgets) ->
+create_toggle_link = (parent, toggle) ->
+  toggle_link = $("<a href='#'>").html "toggle"
+  toggle_link.click toggle
+  parent.append ' '
+  parent.append toggle_link
+
+MultiView = (parent, widgets) ->
+  div = $("<div>")
+  parent.append div
+  parent = div
+  index = 0
   self =
-    current: -> widgets[0]
+    current: -> widgets[index]
+    toggle: ->
+      self.current().element().hide()
+      val = self.current().value()
+      index = (index + 1) % widgets.length
+      self.current().element().show()
+  console.log widgets
+  for widget in widgets
+    widget.element().hide()
+    parent.append widget.element()
+  widgets[0].element().show()
+  create_toggle_link(parent, self.toggle)
+  self
 
 create_save_link = (widget, save_method) ->
   save_link = $("<a href='#'>").html("save")
   save_link.click ->
     save_method widget.value()
   elem = widget.element()
+  elem.append ' '
   elem.append save_link
   
 List = (array, widgetizer, save_method) ->
   elem = $("<div>")
   subwidgets = _.map(array, widgetizer)
-  multi_view = MultiView [
+  self =
+    element: -> elem
+    value: -> multi_view.current().value()
+    append: (subelem) -> elem.append subelem
+    
+  multi_view = MultiView self, [
     ListRawView(array),
     ListEditView(subwidgets),
   ]
 
-  self =
-    element: -> elem
-    value: -> multi_view.current().value()
-    
-  elem.append multi_view.current().element()
   create_save_link(self, save_method)  
   self
     
--- json_wiki.js
+++ json_wiki.js
@@ -1,5 +1,5 @@
 (function() {
-  var Atom, List, ListEditView, ListRawView, MultiView, create_save_link;
+  var Atom, List, ListEditView, ListRawView, MultiView, create_save_link, create_toggle_link;
   Atom = function(s) {
     var elem, self;
     elem = $("<textarea>");
@@ -14,12 +14,14 @@
     };
   };
   ListRawView = function(array) {
-    var self, textarea;
+    var div, self, textarea;
+    div = $("<div>");
     textarea = $("<textarea>").attr("rows", 10);
     textarea.html(JSON.stringify(array, null, " "));
+    div.append(textarea);
     return self = {
       element: function() {
-        return textarea;
+        return div;
       },
       value: function() {
         return JSON.parse(textarea.html());
@@ -45,13 +47,40 @@
       }
     };
   };
-  MultiView = function(widgets) {
-    var self;
-    return self = {
+  create_toggle_link = function(parent, toggle) {
+    var toggle_link;
+    toggle_link = $("<a href='#'>").html("toggle");
+    toggle_link.click(toggle);
+    parent.append(' ');
+    return parent.append(toggle_link);
+  };
+  MultiView = function(parent, widgets) {
+    var div, index, self, widget, _i, _len;
+    div = $("<div>");
+    parent.append(div);
+    parent = div;
+    index = 0;
+    self = {
       current: function() {
-        return widgets[0];
+        return widgets[index];
+      },
+      toggle: function() {
+        var val;
+        self.current().element().hide();
+        val = self.current().value();
+        index = (index + 1) % widgets.length;
+        return self.current().element().show();
       }
     };
+    console.log(widgets);
+    for (_i = 0, _len = widgets.length; _i < _len; _i++) {
+      widget = widgets[_i];
+      widget.element().hide();
+      parent.append(widget.element());
+    }
+    widgets[0].element().show();
+    create_toggle_link(parent, self.toggle);
+    return self;
   };
   create_save_link = function(widget, save_method) {
     var elem, save_link;
@@ -60,22 +89,25 @@
       return save_method(widget.value());
     });
     elem = widget.element();
+    elem.append(' ');
     return elem.append(save_link);
   };
   List = function(array, widgetizer, save_method) {
     var elem, multi_view, self, subwidgets;
     elem = $("<div>");
     subwidgets = _.map(array, widgetizer);
-    multi_view = MultiView([ListRawView(array), ListEditView(subwidgets)]);
     self = {
       element: function() {
         return elem;
       },
       value: function() {
         return multi_view.current().value();
+      },
+      append: function(subelem) {
+        return elem.append(subelem);
       }
     };
-    elem.append(multi_view.current().element());
+    multi_view = MultiView(self, [ListRawView(array), ListEditView(subwidgets)]);
     create_save_link(self, save_method);
     return self;
   };
